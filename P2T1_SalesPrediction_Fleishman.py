8b5c8b4440c2e8a34f7a6c6b044c0511a896aeed
fleishma2363 <36448955+fleishma2363@users.noreply.github.com>
2018-02-19 21:04:57 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_Fleishman.py
@@ -0,0 +1,9 @@
+print("CTI110")
+print("P2T1-Sales Prediction")
+print("Aaron Fleishman")
+print("2/16/18")
+projectedTotalSales = float (input("Please enter the projected amount" + \
+                                   "of total sales: ") )
+profit = 0.23 * projectedTotalSales
+print( "The profit is $" + format( profit, ",.2f"))
+
